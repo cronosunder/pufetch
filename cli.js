c0fb9bcda43b6bb0a847cda626aa9b99db6ef5f4
Claudio Miranda <cronosunder@gmail.com>
2019-07-04 21:43:27 -0400

    aguanta TXT

--- cli.js
+++ cli.js
@@ -2,9 +2,8 @@
 
 'use strict';
 
-const dns = require('dns');
 const isUrl = require('is-url');
-const got = require('got');
+
 const ora = require('ora');
 const jsonFile = require('jsonfile');
 const fs = require('fs');
@@ -16,13 +15,14 @@ const pkg = require('./package.json');
 
 updateNotifier({pkg}).notify();
 
+const {printInfo, checkConnection, urlDeLista,obtenerElementosDeLista} = require('./modulos/utils.js');
 const spinner = ora();
 const arg = process.argv[2];
 const val = process.argv[4];
 var urlLista = process.argv[3];
 
 const setName = `${Math.random().toString(16).substr(10)}`;
-console.log("ARG",process.argv);
+console.log("ARG", process.argv);
 let rad = `${process.argv[5]}` || setName;
 if (rad === 'undefined.json') {
 	rad = setName;
@@ -32,38 +32,9 @@ const {log} = console;
 const end = process.exit;
 const url = 'https://youtube.com/watch?v=';
 
-const checkConnection = () => {
-	dns.lookup('youtube.com', err => {
-		if (err) {
-			logUpdate(`\n${chalk.red('  ✖ ')} ${chalk.dim('Please check your internet connection!')}\n`);
-			end(1);
-		} else {
-			logUpdate();
-			spinner.text = 'Puffing...';
-			spinner.start();
-		}
-	});
-};
 
 if (!arg || arg === '-h' || arg === '--help') {
-	log(`
-	        ${chalk.keyword('orange')('⚡⚡')} ${chalk.blue('Youtube Playlist Link Fetcher')} ${chalk.keyword('orange')('⚡⚡')}
-
- Usage: puf <commands> [url]
-        puf <commands> [url] ${chalk.dim('<command>')}
-
- Commands:
-   -f, ${chalk.dim('--fetch')}             Fetch url of items in the playlist
-   -e, ${chalk.dim('--export')}            Export urls into json
-
- Extra:
-   ${chalk.dim('-f')} url --name           Show links along with the title
-   ${chalk.dim('-e')} url --name <name>    Set desired name of the exported playlist
-
- Help:
-   $ puf -f ${chalk.dim('https://goo.gl/QcSugM')} --name
-   $ puf -e ${chalk.dim('https://goo.gl/QcSugM')} --name course
-		`);
+	printInfo();
 	end(1);
 }
 
@@ -72,16 +43,8 @@ if (!urlLista || isUrl(urlLista) === false) {
 	end(1);
 }
 
-var urlOriginal = urlLista;
-
-urlOriginal=urlOriginal.split("?")[1]
-var partes = urlOriginal.split("&");
-for(var i = 0 ; i< partes.length;i++){
-	if(partes[i].startsWith("list=")){
-		urlLista = " https://www.youtube.com/playlist?"+partes[i];
-	}
-}
-
+urlLista = urlDeLista(urlLista);
+log(urlLista)
 if (arg === '-f' || arg === '--fetch') {
 	checkConnection();
 	got(urlLista).then(res => {
@@ -114,7 +77,7 @@ if (arg === '-f' || arg === '--fetch') {
 
 if (arg === '-e' || arg === '--export') {
 	checkConnection();
-	rad = rad+".json";
+	rad = rad + ".json";
 	got(urlLista).then(res => {
 		const $ = cheerio.load(res.body);
 		const tr = $('tr');
@@ -148,32 +111,39 @@ if (arg === '-e' || arg === '--export') {
 }
 
 if (arg === '-t' || arg === '--txt') {
-	checkConnection();
-	rad = rad+".txt";
-	got(urlLista).then(res => {
-		const $ = cheerio.load(res.body);
-		const tr = $('tr');
-		logUpdate(`\n ${chalk.blue('🍁')}  Done! Playlist exported ${chalk.green(tr.length)} as : \n\n ${chalk.green('🌴')}  ${chalk.yellow(rad)} into ${chalk.blue(process.cwd())}\n`);
-		for (let i = 0; i < tr.length; i++) {
-			const obj = {
-				playlist: []
-			};
-
-			for (let j = 0; j <= i; j++) {
-				obj.playlist.push(url + tr.eq(j).attr('data-video-id')+"\t"+tr.eq(j).attr('data-title'));
-			}
+	rad = rad + ".txt";
+	obtenerElementosDeLista(urlLista).then(obj => {
 			logUpdate(`\n ${chalk.blue('🍁')} La lista tiene  :  ${chalk.green(obj.playlist.length)}  ${chalk.yellow(rad)} into ${chalk.blue(process.cwd())}\n`);
 			fs.writeFile(rad, obj.playlist.join("\n"), {spaces: 2}, err => {
 				end(1);
 				log(err);
 			});
-		}
-
 		spinner.stop();
-	}).catch(error => {
-		if (error) {
-			logUpdate(`${error}`);
-			end(1);
-		}
 	});
+	// got(urlLista).then(res => {
+	// 	const $ = cheerio.load(res.body);
+	// 	const tr = $('tr');
+	// 	logUpdate(`\n ${chalk.blue('🍁')}  Done! Playlist exported ${chalk.green(tr.length)} as : \n\n ${chalk.green('🌴')}  ${chalk.yellow(rad)} into ${chalk.blue(process.cwd())}\n`);
+	// 	for (let i = 0; i < tr.length; i++) {
+	// 		const obj = {
+	// 			playlist: []
+	// 		};
+	//
+	// 		for (let j = 0; j <= i; j++) {
+	// 			obj.playlist.push(url + tr.eq(j).attr('data-video-id') + "\t" + tr.eq(j).attr('data-title'));
+	// 		}
+	// 		logUpdate(`\n ${chalk.blue('🍁')} La lista tiene  :  ${chalk.green(obj.playlist.length)}  ${chalk.yellow(rad)} into ${chalk.blue(process.cwd())}\n`);
+	// 		fs.writeFile(rad, obj.playlist.join("\n"), {spaces: 2}, err => {
+	// 			end(1);
+	// 			log(err);
+	// 		});
+	// 	}
+	//
+	// 	spinner.stop();
+	// }).catch(error => {
+	// 	if (error) {
+	// 		logUpdate(`${error}`);
+	// 		end(1);
+	// 	}
+	// });
 }
